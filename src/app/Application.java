df73174927f42a2f9e98a067bbc5db85d23dc29d
Gabriel <gabriel.beer@studentfr.ch>
2024-09-04 09:23:05 +0200

    fin

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,26 +1,24 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.ArrayList;
+import app.models.Personne;
+
 public class Application {
 
     public static void main(String[] args) {
-
-        //  --------------------------------------------------        
-        //           __     _____ _____ ____  _____ 
-        //           \ \   / / _ \_   _|  _ \| ____|
-        //            \ \ / / | | || | | |_) |  _|  
-        //             \ V /| |_| || | |  _ <| |___ 
-        //              \_/__\___/_|_|_|_| \_\_____|
-        //                / ___/ _ \|  _ \| ____|   
-        //               | |  | | | | | | |  _|     
-        //               | |__| |_| | |_| | |___    
-        //                \____\___/|____/|_____|   
-        //                    |_ _/ ___|_ _|        
-        //                     | | |    | |         
-        //                     | | |___ | |         
-        //                    |___\____|___|        
-        //                                                  
-        //  ---------------------------------------------------
-
+        ArrayList<Personne> listePersonne = new ArrayList<Personne>();
+        Personne personne1 = new Personne("Jean", "Jean", "a", "a", 1000);
+        Personne personne2 = new Personne("Luc", "Luc", "b", "b", 2000);
+        listePersonne.add(personne1);
+        listePersonne.add(personne2);
+        for (Personne personne : listePersonne) {
+            System.out.println(personne.getNom());
+            System.out.println(personne.getPrenom());
+            System.out.println(personne.getRue());
+            System.out.println(personne.getLocalite());
+            System.out.println(personne.getNpa());
+        }
     }
 
 }
